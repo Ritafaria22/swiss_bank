9f96ebb630d8f5d59893bb51c1aa3d9ebf8baa66
Ritafaria22 <fariakabir507@gmail.com>
2025-03-11 23:38:43 +0600

    database added

--- swiss_bank/settings.py
+++ swiss_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -92,15 +93,23 @@ WSGI_APPLICATION = 'swiss_bank.wsgi.application'
 
 
  
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': env("DB_NAME"),
+#         'USER': env("DB_USER"),
+#         'PASSWORD': env("DB_PASSWORD"),
+#         'HOST': env("DB_HOST"),
+#         'PORT': env("DB_PORT"),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env("DB_NAME"),
-        'USER': env("DB_USER"),
-        'PASSWORD': env("DB_PASSWORD"),
-        'HOST': env("DB_HOST"),
-        'PORT': env("DB_PORT"),
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://swissbank_user:FiqI3umzGGTpRsqBAbhgypVg3OkFQpRt@dpg-cv878ttumphs738be5i0-a.oregon-postgres.render.com/swissbank',
+        
+    )
 }
 
 
